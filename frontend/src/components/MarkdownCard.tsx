572103f7c0e6bb1ef53f5ed243970961cf185cfb
Rohit Patnaik <rohitptnk03@gmail.com>
2026-03-12 16:36:57 +0530

    Improve Code Rendering

--- frontend/src/components/MarkdownCard.tsx
+++ frontend/src/components/MarkdownCard.tsx
@@ -1,4 +1,6 @@
 import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 
 type Props = {
@@ -8,7 +10,29 @@ type Props = {
 export default function MarkdownCard({ content }: Props) {
     return (
         <div className="markdown-card">
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <ReactMarkdown
+                components={{
+                    code(props) {
+                        const { children, className, node, ref, ...rest } = props
+                        const match = /language-(\w+)/.exec(className || '')
+                        return match ? (
+                            <SyntaxHighlighter
+                                PreTag="div"
+                                language={match[1]}
+                                style={vscDarkPlus}
+                            >
+                                {String(children).replace(/\n$/, '')}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <code {...rest} className={className} ref={ref}>
+                                {children}
+                            </code>
+                        )
+                    }
+                }}
+            >
+                {content}
+            </ReactMarkdown>
         </div>
     )
-}
+}
